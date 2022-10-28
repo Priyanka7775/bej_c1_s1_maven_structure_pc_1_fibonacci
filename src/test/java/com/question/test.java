b2ee7a6f179c031d07c473e4ea2a5563a04c19ee
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-10-28 21:19:04 +0530

    pc1

---
+++ src/test/java/com/question/test.java
@@ -0,0 +1,32 @@
+package com.question;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class test {
+    fabonacci fab=null;
+    private TestCase Assertions;
+
+    @Before
+    public  void setup(){
+        fab=new fabonacci();
+    }
+    @After
+    public void tearDown(){
+        fab = null;
+    }
+
+
+    @Test
+    public void getFabonacciNum(){
+        Assertions.assertEquals(21,fab.fabonacci(8));
+    }
+
+
+
+
+
+
+}
